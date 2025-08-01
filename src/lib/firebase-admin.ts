83a845c2d4409a57d04bb757539687b21b32fc32
onaneyeayodeji <onaneyeayodeji@gmail.com>
2025-08-01 19:17:35 +0100

    changes

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,10 +2,11 @@
 import { cert, getApps, initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
-
-// Ensure the private key is correctly formatted
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+};
 
 if (!getApps().length) {
   initializeApp({
