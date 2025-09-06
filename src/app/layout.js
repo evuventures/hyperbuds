9509be8d5ac5c736c116ebf93dacffde58293494
Alayande Esther <ealayande073@stu.ui.edu.ng>
2025-09-06 19:42:26 +0100

    modified layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "../components/AuthProvider";
+import ClientLayout from "./ClientLayout"; // <-- Import the new client component
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <ClientLayout>{children}</ClientLayout> {/* <-- Use the new client component */}
       </body>
     </html>
   );
