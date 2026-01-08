ae094768ef1734ff29966c11bb4ceaf05df658f0
Alayande Esther <alayandeesther469@gmail.com>
2026-01-08 16:14:00 +0100

    solved build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,16 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {}, // 🔥 enable Turbopack
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com', 
+        hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
       },
     ],
-  },
-  webpack(config, { dev }) {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-        ignored: ['**/node_modules', '**/.git'],
-      };
-    }
-    return config;
   },
 };
 
